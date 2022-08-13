148bb780bd9fe88d107d5e11a16151c571a2b80a
trungtq90 <111162482+trungtq90@users.noreply.github.com>
2022-08-13 08:31:09 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+basic.showString("Wifi")
+ESP8266_IoT.initWIFI(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
+ESP8266_IoT.connectWifi("Redmi K30", "12345678")
+ESP8266_IoT.wait(5000)
+if (ESP8266_IoT.wifiState(true)) {
+    basic.showString("Wifi Connected!")
+    ESP8266_IoT.adafruit_setting("trungtq90", "aio_yWFl661i31Rg8k4EHCHGR88bpKNr")
+} else {
+	
+}
 basic.forever(function () {
 	
 })
